6de1909e504150063ec76de27a8bb968250572d2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 16:30:55 -0700

    seems to fail inside a loop but work from a terminal

--- eBook-editing/install-calibre.bash
+++ eBook-editing/install-calibre.bash
@@ -1,5 +1,6 @@
 #! /bin/bash -v
 
 # install from Linux binary
+rm -fr /opt/calibre
 source /etc/profile
 python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='/opt')"
