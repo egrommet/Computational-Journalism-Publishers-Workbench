7360f7726688147249847e0127bb2066588d807d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-01 23:38:46 -0700

    all tested on Ubuntu (minus graphviz)

--- apt-get-dependencies.bash
+++ apt-get-dependencies.bash
@@ -6,33 +6,51 @@ apt-get install -y \
   vim-gtk \
   git \
   curl
-  #perl-tk \
-  #wget \
-  #libdigest-md5-file-perl
-##! /bin/bash -v
-#
-#yum install -y \
-#  gcc-fortran \
-#  gcc-c++ \
-#  java-1.7.0-openjdk-devel
-#
-##  readline-devel \
-##  xorg-x11-libXt-devel \
-##  xorg-x11-libX11-devel \
-##  texinfo \
-##  tk-devel \
-##  pango-devel \
-##  cairo-devel \
-##  libicu-devel \
-##  libxml2-devel \
-##  graphviz-devel \
-##  openmpi-devel \
-##  gtk2-devel \
-##  gd-devel \
-##  libcurl-devel \
-##  fftw3-devel \
-##  freeglut-devel \
-##  gsl-devel \
-##  libsndfile-devel \
-##  wordnet-devel
-##cat WNHOME.txt >> /etc/profile.local
+
+# R-compiled
+apt-get install -y \
+  gfortran \
+  g++ \
+  openjdk-7-jdk \
+  libreadline-gplv2-dev \
+  libxt-dev \
+  libx11-dev \
+  libxmu-dev \
+  libpango1.0-dev \
+  libcairo2-dev \
+  tk-dev \
+  libtiff4-dev \
+  texinfo \
+  libicu-dev
+
+# ggobi
+apt-get install -y \
+  libgtk2.0-dev \
+  libxml2-dev \
+  libltdl-dev
+
+# Packages
+apt-get install -y \
+  freeglut3-dev \
+  libfftw3-dev \
+  libgd2-xpm-dev \
+  libgsl0-dev \
+  libsndfile1-dev
+
+# RStudio
+apt-get install -y \
+  libqtwebkit-dev \
+  build-essential \
+  pkg-config \
+  fakeroot \
+  cmake \
+  uuid-dev \
+  libssl-dev \
+  libbz2-dev \
+  zlib1g-dev \
+  libpam-dev \
+  libapparmor1  \
+  apparmor-utils \
+  libboost-all-dev \
+  ant \
+  unzip
