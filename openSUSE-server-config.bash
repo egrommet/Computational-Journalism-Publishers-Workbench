6bc31bab94dcac15a76009e3018ce34fdbcc54c4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 08:41:19 -0700

    needs to be tested but "should work"

---
+++ openSUSE-server-config.bash
@@ -0,0 +1,14 @@
+#! /bin/bash -v
+
+useradd -r rstudio-server
+cp /usr/local/lib/rstudio-server/extras/pam/rstudio /etc/pam.d
+cp /usr/local/lib/rstudio-server/extras/init.d/redhat/rstudio-server /etc/init.d
+chmod +x /etc/init.d/rstudio-server
+/sbin/chkconfig --add rstudio-server
+ln -f -s /usr/local/lib/rstudio-server/bin/rstudio-server \
+  /usr/sbin/rstudio-server
+rstudio-server start
+
+echo "Open TCP port 8787"
+yast2 firewall
+firefox localhost:8787 &
