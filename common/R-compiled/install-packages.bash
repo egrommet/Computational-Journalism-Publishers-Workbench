1e32129439cf9efebd215b0d0eca30cd6dfa4eb9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-01 22:53:54 -0700

    all tested - leaving Rgraphviz out for now

--- common/R-compiled/install-packages.bash
+++ common/R-compiled/install-packages.bash
@@ -6,3 +6,5 @@ export DISPLAY=:0.0
 export PATH=$PATH:/usr/local/bin
 /usr/bin/time R --no-save --no-restore < load-packages.R
 cp wordnet.sh /etc/profile.d # set WordNet home
+source /etc/profile
+set|grep WNHOME
