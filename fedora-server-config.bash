86d75b36a49a1f1495b8684cbf2b8c201798d8f2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 08:40:04 -0700

    tested

--- fedora-server-config.bash
+++ fedora-server-config.bash
@@ -2,10 +2,13 @@
 
 useradd -r rstudio-server
 cp /usr/local/lib/rstudio-server/extras/pam/rstudio /etc/pam.d
-cp /usr/local/lib/rstudio-server/extras/init.d/suse/rstudio-server /etc/init.d
+cp /usr/local/lib/rstudio-server/extras/init.d/redhat/rstudio-server /etc/init.d
 chmod +x /etc/init.d/rstudio-server
 /sbin/chkconfig --add rstudio-server
 ln -f -s /usr/local/lib/rstudio-server/bin/rstudio-server \
   /usr/sbin/rstudio-server
 rstudio-server start
-echo 'TODO: find Ubuntu firewall; modify for upstart'
+
+echo "Open TCP port 8787"
+system-config-firewall
+firefox localhost:8787 &
