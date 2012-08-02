01da35bb83afbdd4970f750a15120d87cde1e135
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 15:11:47 -0700

    figure out who the installer i!

---
+++ common/install-dependencies.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -v
+
+if [ -e "/usr/bin/apt-get" ]
+then
+  ./apt-get-dependencies.bash
+elif [ -e "/usr/bin/yum" ]
+then
+  ./yum-dependencies.bash
+elif [ -e "/usr/bin/zypper" ]
+then
+  ./zypper-dependencies.bash
+fi
