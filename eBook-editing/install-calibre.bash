2264e018eff065ea624aac83d5cef1e060a09a09
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 15:39:14 -0700

    upstream recommends binary install

--- eBook-editing/install-calibre.bash
+++ eBook-editing/install-calibre.bash
@@ -3,5 +3,5 @@
 mkdir -p /usr/local/src
 pushd /usr/local/src
 
-wget -L http://calibre-ebook.googlecode.com/files/calibre-0.8.62.tar.xz
-tar xf calibre-0.8.62.tar.xz
+# install from Linux binary
+python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='/usr/local/calibre')"
