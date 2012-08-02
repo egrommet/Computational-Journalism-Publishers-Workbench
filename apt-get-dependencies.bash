411a84dd00964fc1384d63d48dab77cfd3e947af
M. Edward (Ed) Borasky <znmeb@Ubuntu-Test.(none)>
2012-08-01 20:23:32 -0700

    fleshing out in test VM

--- apt-get-dependencies.bash
+++ apt-get-dependencies.bash
@@ -1,36 +1,38 @@
 #! /bin/bash -v
 
+# TexLive
 apt-get install -y \
-  curl \
   vim \
   vim-gtk \
-  perl-tk \
-  wget \
-  libdigest-md5-file-perl
-#! /bin/bash -v
-
-yum install -y \
-  gcc-fortran \
-  gcc-c++ \
-  java-1.7.0-openjdk-devel
-
-#  readline-devel \
-#  xorg-x11-libXt-devel \
-#  xorg-x11-libX11-devel \
-#  texinfo \
-#  tk-devel \
-#  pango-devel \
-#  cairo-devel \
-#  libicu-devel \
-#  libxml2-devel \
-#  graphviz-devel \
-#  openmpi-devel \
-#  gtk2-devel \
-#  gd-devel \
-#  libcurl-devel \
-#  fftw3-devel \
-#  freeglut-devel \
-#  gsl-devel \
-#  libsndfile-devel \
-#  wordnet-devel
-#cat WNHOME.txt >> /etc/profile.local
+  git \
+  curl
+  #perl-tk \
+  #wget \
+  #libdigest-md5-file-perl
+##! /bin/bash -v
+#
+#yum install -y \
+#  gcc-fortran \
+#  gcc-c++ \
+#  java-1.7.0-openjdk-devel
+#
+##  readline-devel \
+##  xorg-x11-libXt-devel \
+##  xorg-x11-libX11-devel \
+##  texinfo \
+##  tk-devel \
+##  pango-devel \
+##  cairo-devel \
+##  libicu-devel \
+##  libxml2-devel \
+##  graphviz-devel \
+##  openmpi-devel \
+##  gtk2-devel \
+##  gd-devel \
+##  libcurl-devel \
+##  fftw3-devel \
+##  freeglut-devel \
+##  gsl-devel \
+##  libsndfile-devel \
+##  wordnet-devel
+##cat WNHOME.txt >> /etc/profile.local
