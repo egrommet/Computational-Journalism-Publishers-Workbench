71b7e2bea57bb9b91dd87edf63038452f5f6ab95
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-03 12:37:07 -0700

    need to check webkit devel name!

---
+++ RStudio/zypper-dependencies.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -v
+
+zypper install -y \
+  libqt4-webkit-devel \
+  build \
+  cmake \
+  libbz2-devel \
+  zlib-devel \
+  libuuid-devel \
+  libopenssl-devel \
+  pam-devel \
+  boost-devel \
+  ant
