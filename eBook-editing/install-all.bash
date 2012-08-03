2dc716743e7ff6ec81955f164f23ca9b6c374b53
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-02 22:33:05 -0700

    calibre install still doesn't work inside install-all

--- eBook-editing/install-all.bash
+++ eBook-editing/install-all.bash
@@ -1,6 +1,7 @@
 #! /bin/bash -v
 
-for i in dependencies pandoc calibre epubcheck beta-sigil
+#for i in dependencies pandoc calibre epubcheck beta-sigil
+for i in calibre
 do
   ./install-${i}.bash 2>&1 | tee ${i}.log
 done
--- eBook-editing/install-calibre.bash
+++ eBook-editing/install-calibre.bash
@@ -3,4 +3,6 @@
 # install from Linux binary
 rm -fr /opt/calibre
 source /etc/profile
+set|grep PATH
+which python
 python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='/opt')"
