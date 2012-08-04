9c52bc47f358e5dc9c5d33360c6b8b18da4b6041
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-03 20:36:06 -0700

    ditching freemind - pulls in openJDK 1.6 :-(

--- eBook-editing/apt-get-dependencies.bash
+++ eBook-editing/apt-get-dependencies.bash
@@ -3,10 +3,4 @@
 # Sigil beta
 apt-get install -y \
   zlib-bin \
-  freemind \
-  freemind-browser \
-  freemind-doc \
-  freemind-plugins-help \
-  freemind-plugins-script \
-  freemind-plugins-svg \
   vym
