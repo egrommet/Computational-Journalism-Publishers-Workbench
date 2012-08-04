66ba8fe2eed8ff2c626cf6863c3586ebf30e008c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-03 22:27:45 -0700

    added R CMD javareconf

--- R-compiled/install-packages.bash
+++ R-compiled/install-packages.bash
@@ -4,6 +4,7 @@ xhost +
 source /etc/profile # reload environment variables
 export DISPLAY=:0.0
 export PATH=$PATH:/usr/local/bin
+R CMD javareconf
 /usr/bin/time R --no-save --no-restore < load-packages.R
 cp wordnet.sh /etc/profile.d # set WordNet home
 source /etc/profile
