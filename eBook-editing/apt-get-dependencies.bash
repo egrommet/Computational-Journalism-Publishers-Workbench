cfdd7483f950d552f183944e0a4aa37bf94f6070
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-03 20:30:00 -0700

    added freemind and vym

--- eBook-editing/apt-get-dependencies.bash
+++ eBook-editing/apt-get-dependencies.bash
@@ -2,4 +2,11 @@
 
 # Sigil beta
 apt-get install -y \
-  zlib-bin
+  zlib-bin \
+  freemind \
+  freemind-browser \
+  freemind-doc \
+  freemind-plugins-help \
+  freemind-plugins-script \
+  freemind-plugins-svg \
+  vym
