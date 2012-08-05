b038297517942f91c8aeebf40f3441968388ef87
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-05 03:13:28 -0700

    lyx from source

--- eBook-editing/install-all.bash
+++ eBook-editing/install-all.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for i in dependencies pandoc calibre vym epubcheck beta-sigil
+for i in dependencies pandoc calibre vym epubcheck beta-sigil lyx
 do
   ./install-${i}.bash 2>&1 | tee ${i}.log
 done
--- eBook-editing/yum-dependencies.bash
+++ eBook-editing/yum-dependencies.bash
@@ -2,4 +2,11 @@
 
 # Sigil beta
 yum install -y \
+  libreoffice-writer2latex \
+  libreoffice-writer2xhtml \
+  libreoffice-pdfimport \
+  latex2html \
+  latex2rtf \
+  unoconv \
+  jabref \
   minizip
