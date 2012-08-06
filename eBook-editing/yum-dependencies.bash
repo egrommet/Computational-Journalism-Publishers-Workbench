08a86560c8c3a667daca5473323aa80281c58142
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-05 23:18:28 -0700

    ditched LibreOffice stuff

--- eBook-editing/yum-dependencies.bash
+++ eBook-editing/yum-dependencies.bash
@@ -11,11 +11,7 @@
 
 # Sigil beta
 yum install -y \
-  libreoffice-writer2latex \
-  libreoffice-writer2xhtml \
-  libreoffice-pdfimport \
   latex2html \
   latex2rtf \
-  unoconv \
   jabref \
   minizip
