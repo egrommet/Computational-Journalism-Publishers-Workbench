4117ac6f511e1ca69d4a746366b85eaa60021da1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-05 20:14:10 -0700

    script to "publish" the eBook

---
+++ Book/save-html.bash
@@ -0,0 +1,38 @@
+#! /bin/bash -v
+
+# make directory
+mkdir -p images
+
+# make html
+rm -fr Book.html.LyXconv
+lyx -e html Book.lyx
+rm -f *.tex
+
+# ImageMagick FTW!
+pushd Book.html.LyXconv
+for i in *.png
+do
+  cp $i zztemp.png
+  convert zztemp.png -resize 500x600 $i
+done
+popd
+
+# Add metadata and make EPUB with Sigil
+rm -f *.epub
+pushd Book.html.LyXconv
+sigil *.html
+popd
+
+# run through epubcheck
+epub-fix --unmanifested --epubcheck Book.epub
+epubcheck Book.epub
+
+# stash stuff here in Git
+git add *.lyx
+git commit
+git push
+
+# cleanup
+rm -f \#*
+rm -f *.lyx~
+git status
