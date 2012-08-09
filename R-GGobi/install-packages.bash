703204d3f0f01fc8fcdf3bb7fa4ef260130e1d2d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-09 01:10:12 -0700

    R Commander desktop entry

--- R-GGobi/install-packages.bash
+++ R-GGobi/install-packages.bash
@@ -15,3 +15,6 @@ export DISPLAY=:0.0
 export PATH=$PATH:/usr/local/bin
 R CMD javareconf
 /usr/bin/time R --no-save --no-restore < load-packages.R
+
+cp Rcmdr.desktop /usr/local/share/applications
+cp Rcmdr.svg /usr/local/share/pixmaps
