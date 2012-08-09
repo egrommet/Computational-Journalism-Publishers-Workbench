e5759970b32efce421335900d3e44cb91e721a16
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-08 23:14:55 -0700

    wordnet logic moved

--- R-GGobi/install-packages.bash
+++ R-GGobi/install-packages.bash
@@ -15,6 +15,3 @@ export DISPLAY=:0.0
 export PATH=$PATH:/usr/local/bin
 R CMD javareconf
 /usr/bin/time R --no-save --no-restore < load-packages.R
-cp wordnet.sh /etc/profile.d # set WordNet home
-source /etc/profile
-set|grep WNHOME
