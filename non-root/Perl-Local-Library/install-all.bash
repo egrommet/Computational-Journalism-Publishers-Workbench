41699766af364edc19632a98b1ad008f120ae3ca
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-11 12:23:37 -0700

    backslashes

--- non-root/Perl-Local-Library/install-all.bash
+++ non-root/Perl-Local-Library/install-all.bash
@@ -1,4 +1,13 @@
 #! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
 
 # clean slate
 rm -fr ~/perl5 ~/.cpan
@@ -11,11 +20,11 @@ set|grep PERL
 cpan < config.cpan 2>&1 | tee config.log # do the autoconfig
 
 for i in 
-  CPAN
-  Net::Twitter
-  AnyEvent::Twitter::Stream
-  Sys::Statistics::Linux
-  Hash::Flatten
+  CPAN \
+  Net::Twitter \
+  AnyEvent::Twitter::Stream \
+  Sys::Statistics::Linux \
+  Hash::Flatten \
   Browser::Open
 do
   cpan ${i} 2>&1 | tee ${i}.log
