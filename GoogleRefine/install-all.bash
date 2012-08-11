e2973bbd5e5c5f634a234a48826fce4a6141a07e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-10 21:09:36 -0700

    Google Refine

---
+++ GoogleRefine/install-all.bash
@@ -0,0 +1,3 @@
+#! /bin/bash -v
+
+./install-google-refine.bash 2>&1 | tee google-refine.log
---
+++ GoogleRefine/install-google-refine.bash
@@ -0,0 +1,17 @@
+#! /bin/bash -v
+export WHERE=http://google-refine.googlecode.com/files
+export DIR=google-refine-2.5
+export VERSION=${DIR}-r2407
+export WHAT=${VERSION}.tar.gz
+
+mkdir -p ${HOME}/local
+pushd ${HOME}/local
+rm -fr ${WHAT} ${DIR}
+wget ${WHERE}/${WHAT}
+tar xf ${WHAT}
+rm ${WHAT}
+popd
+
+mkdir -p ${HOME}/.local/share/applications
+sed "s;\$BASE;${HOME}/local/${DIR};" google-refine.desktop \
+  > ${HOME}/.local/share/applications/google-refine.desktop
