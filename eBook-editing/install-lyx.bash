87eceb6c0e9ff1b8c4fb1becd7c19af3c7faf409
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-18 21:52:21 -0700

    figure out latest version

--- eBook-editing/install-lyx.bash
+++ eBook-editing/install-lyx.bash
@@ -12,7 +12,7 @@
 mkdir -p /usr/local/src
 pushd /usr/local/src
   export WHERE=ftp://ftp.lyx.org/pub/lyx/stable/2.0.x
-  export VERSION=2.0.4
+  export VERSION=`curl http://www.lyx.org/Download|grep -e 'tar.xz'|sed 's/^.*lyx-//'|sed 's/\.tar.*$//'`
   export DIR=lyx-${VERSION}
   export WHAT=${DIR}.tar.xz
   curl ${WHERE}/${WHAT} > ${WHAT}
