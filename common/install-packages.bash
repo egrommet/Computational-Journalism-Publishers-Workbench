ba39fac3d753ba8c5c384f5c85e498718707e95d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-08-23 20:13:00 -0700

    refactored

---
+++ common/install-packages.bash
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+xhost +
+source /etc/profile # reload environment variables
+export DISPLAY=:0.0
+export PATH=$PATH:/usr/local/bin
+R CMD javareconf
+/usr/bin/time R --no-save --no-restore < load-packages.R
