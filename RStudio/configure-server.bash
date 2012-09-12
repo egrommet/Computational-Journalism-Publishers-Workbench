d27f99ce809fab1f7c9fae9f041a2b5376fced01
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-09-12 00:20:53 -0700

    server configuration

---
+++ RStudio/configure-server.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+if [ -e "/usr/bin/apt-get" ]
+then
+  ./ubuntu-server-config.bash
+elif [ -e "/usr/bin/yum" ]
+then
+  ./fedora-server-config.bash
+elif [ -e "/usr/bin/zypper" ]
+then
+  ./openSUSE-server-config.bash
+fi
