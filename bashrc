598f857e7d1e9f3199924d7b13b1537e73d18b25
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-10-30 13:52:15 -0700

    ready for testing

--- bashrc
+++ bashrc
@@ -1,3 +1,4 @@
 alias l='ls --color=none -CF'
 alias ll='l -ltr'
 alias la='ll -A'
+export PATH=${HOME}/local/bin:${PATH}
--- install-all.bash
+++ install-all.bash
@@ -9,8 +9,12 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# get babysitting out of the way for openSUSE :-(
+# set up stuff everyone needs
+mkdir -p ${HOME}/local/src
+cp bashrc ~/.bashrc
+source ~/.bashrc
 
+# get babysitting out of the way for openSUSE :-(
 if [ -e "/usr/bin/zypper" ]
 then
   ./zypper-repositories.bash
