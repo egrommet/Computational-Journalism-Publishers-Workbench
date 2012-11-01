5a18109df71e1a7bd2ff0851ae41200f8846fe22
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-10-31 18:29:34 -0700

    PostgreSQL and pgadmin3 - needs ports for apt-get and zypper!

---
+++ PostgreSQL/zypper-dependencies.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+sudo zypper install -y \
+  perl-Crypt-SSLeay \
+  perl-Net-SSLeay \
+  perl-YAML \
+  perl-YAML-Syck \
+  perl-JSON \
+  perl-JSON-XS \
+  perl-PadWalker \
+  perl-local-lib
