93a113b2ef589e7fa5c7fee473f0c074ecb38d59
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-11-09 23:04:37 -0800

    moar number crunchies

---
+++ Libraries/yum-dependencies.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+sudo yum install -y \
+  atlas \
+  atlas-devel \
+  gsl \
+  gsl-devel
