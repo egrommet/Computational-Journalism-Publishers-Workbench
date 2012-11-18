2831f86deb21d029b96e73e98f796069b0ff8f13
M. Edward (Ed) Borasky <znmeb@znmeb.com>
2012-11-17 19:21:53 -0800

    factoring out the tools needed to install VirtualBox guest additions

---
+++ install-devtools.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+echo 'Installing devtools packages'
+echo 'You will need to authenticate'
+if [ -e "/usr/bin/apt-get" ]
+then
+  ./apt-get-devtools.bash 2>&1 | tee platform.log
+elif [ -e "/usr/bin/yum" ]
+then
+  ./yum-devtools.bash 2>&1 | tee platform.log
+elif [ -e "/usr/bin/zypper" ]
+then
+  ./zypper-devtools.bash 2>&1 | tee platform.log
+elif [ -e "/usr/sbin/urpmi" ]
+then
+  ./urpmi-devtools.bash 2>&1 | tee platform.log
+fi
