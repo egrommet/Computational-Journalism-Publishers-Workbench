721260a4391a9a032054922250d7f011d4128220
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-11-18 14:37:27 -0800

    remove installed guest tools

---
+++ apt-get-remove-guest-tools.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+echo "Removing guest tools / additions already installed"
+echo "You will need to authenticate"
+sudo apt-get remove -y \
+  virtualbox-guest-dkms \
+  virtualbox-guest-source \
+  virtualbox-guest-utils \
+  virtualbox-guest-x11
