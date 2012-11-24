a890d207836b1f0db7458d6e2855dab9ef5bc8ee
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-11-23 18:36:00 -0800

    bug fix release upstream

--- eBook-editing/install-freemind.bash
+++ eBook-editing/install-freemind.bash
@@ -12,9 +12,9 @@
 rm -fr ${HOME}/local/share/freemind*
 mkdir -p ${HOME}/local/share/freemind
 pushd ${HOME}/local/share
-curl -L http://downloads.sourceforge.net/project/freemind/freemind-unstable/1.0.0_Beta8/freemind-bin-max-1.0.0_Beta_8.zip > freemind-bin-max-1.0.0_Beta_8.zip
+curl -L http://downloads.sourceforge.net/project/freemind/freemind-unstable/1.0.0_Beta9/freemind-bin-max-1.0.0_Beta_9.zip > freemind-bin-max-1.0.0_Beta_9.zip
 cd freemind
-unzip ../freemind-bin-max-1.0.0_Beta_8.zip
+unzip ../freemind-bin-max-1.0.0_Beta_9.zip
 popd
 
 chmod +x ${HOME}/local/share/freemind/freemind.sh
