81f9803d43ca6206b0f7624b2e3a2768acc98df6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-11-29 21:38:13 -0800

    script to install VMware Tools

---
+++ VMware-setup.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+echo "If you haven't already, do 'VM -> Install VMware Tools'"
+sleep 15
+
+echo "Installing guest additions"
+sudo mkdir -p /mnt/cdrom
+sudo mount /dev/sr0 /mnt/cdrom
+tar xf /mnt/cdrom/VMwareTools-9.2.2-893683.tar.gz
+cd vmware-tools-distrib
+sudo ./vmware-install.pl
+
+echo "Shutting down in 15 seconds so you can take a snapshot"
+sleep 15
+sudo /sbin/shutdown -P now
