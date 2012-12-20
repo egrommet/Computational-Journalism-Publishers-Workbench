99390403721e014ddafca0bf634b4a5f8fef3722
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-12-19 19:25:16 -0800

    too much RAM used on a 1.5 GB VM

--- Redis-Source/benchmark-redis.bash
+++ Redis-Source/benchmark-redis.bash
@@ -14,11 +14,17 @@ source ~/.bash_profile
 iostat -cdmxt -p ALL 2 > iostat.log & # start data collector
 ../Profiling/log-pmaps.bash redis-server > pmaps.log & # process maps
 redis-server ./redis.conf & # start the server
-sudo cpupower frequency-set -r -g performance
-cpupower frequency-info
+if [ -e "/usr/bin/apt-get" ]
+then
+  sudo cpufreq-set -r -g performance
+  cpufreq-info
+else
+  sudo cpupower frequency-set -r -g performance
+  cpupower frequency-info
+fi
 sleep 15 # give server time to stabilize
 #redis-benchmark -c 50 -n 10000 -q --csv | tee redis-benchmark.csv
-redis-benchmark -c 50 -n 200000 -q --csv | tee redis-benchmark.csv
+redis-benchmark -c 50 -n 100000 -q --csv | tee redis-benchmark.csv
 redis-cli < slowlog.cmd > slowlog.log
 ./parse-slowlog.pl slowlog.log > slowlog.csv
 pkill redis-server
