84a7f15b8adcfa0096089307ee50a4d052cb404a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2012-12-27 17:22:13 -0800

    reference is now vmdk

--- Qemu-Scripts/convert.bash
+++ Qemu-Scripts/convert.bash
@@ -8,8 +8,8 @@
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
-/usr/bin/time sudo qemu-img convert -O vmdk -o compat6 $1.qcow2 $1.vmdk
-/usr/bin/time sudo qemu-img convert -O vdi $1.qcow2 $1.vdi
-/usr/bin/time sudo qemu-img convert -O vpc $1.qcow2 $1.vpc
-qemu-info $1.*
-ls -l $1.*
+for i in qcow2 vdi vpc
+do
+  /usr/bin/time qemu-img convert -O ${i} $1.vmdk $1.${i}
+  qemu-info $1.${i}
+done
