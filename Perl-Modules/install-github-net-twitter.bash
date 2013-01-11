079959370918c0c35abcaf745c845b4c461c54b7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-10 22:37:50 -0800

    Install the developer version of Net::Twitter from Github

---
+++ Perl-Modules/install-github-net-twitter.bash
@@ -0,0 +1,19 @@
+#! /bin/bash -v
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+source ~/.bash_profile
+pushd ~/local/src
+rm -fr Net-Twitter
+git clone git://github.com/semifor/Net-Twitter.git
+cd Net-Twitter
+git checkout api-v1.1
+perl Makefile.PL
+popd
