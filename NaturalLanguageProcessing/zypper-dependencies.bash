eef12dc51632226823d562d857753e1c8746af41
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-12 20:25:19 -0800

    NLTK added

--- NaturalLanguageProcessing/zypper-dependencies.bash
+++ NaturalLanguageProcessing/zypper-dependencies.bash
@@ -9,8 +9,16 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+# Repository for NLTK
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_12.2/devel:languages:python.repo
+
+echo "Answer 'a' to 'Do you want to reject the key, trust temporarily, or trust always?' below."
+sudo zypper --gpg-auto-import-keys refresh 
+
 # Packages
 sudo zypper install -y \
+  python-nltk \
   wordnet \
   wordnet-devel
 
