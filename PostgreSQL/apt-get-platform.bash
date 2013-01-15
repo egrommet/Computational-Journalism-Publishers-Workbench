e257812d31b96283a3101a20ea6459807b40f77a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-14 16:03:54 -0800

    first draft of PostgreSQL platform addition

---
+++ PostgreSQL/apt-get-platform.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+sudo apt-get install -y \
+  pgadmin3 \
+  postgresql \
+  postgresql-contrib \
+  postgresql-devel \
+  postgresql-plperl \
+  postgresql-plpython \
+  postgresql-server
