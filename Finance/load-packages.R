c02a0003566de3439c38d435ae2c8f19fb9e730b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-14 21:09:08 -0800

    refactoring / fixes for issues #140, #141, #146

--- Finance/load-packages.R
+++ Finance/load-packages.R
@@ -7,6 +7,21 @@
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
+install.packages(
+  c(
+    'TScompare',
+    'TSdata',
+    'TSPostgreSQL',
+    'TSSQLite',
+    'TShistQuote',
+    'TSgetSymbol',
+    'TSxls',
+    'TSzip',
+    'TSpadi'
+  ),
+  repos='http://cran.fhcrc.org'
+)
+warnings()
 library(ctv)
 update.views(
   c(
