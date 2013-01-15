59f354ced3182e06e0ec14a8be13c391b395c6de
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-14 18:12:13 -0800

    tested on Linux Mint

--- PostgreSQL/apt-get-platform.bash
+++ PostgreSQL/apt-get-platform.bash
@@ -12,8 +12,9 @@
 sudo apt-get install -y \
   pgadmin3 \
   postgresql \
+  postgresql-client \
   postgresql-contrib \
-  postgresql-devel \
+  postgresql-doc \
   postgresql-plperl \
   postgresql-plpython \
-  postgresql-server
+  postgresql-server-dev-all
