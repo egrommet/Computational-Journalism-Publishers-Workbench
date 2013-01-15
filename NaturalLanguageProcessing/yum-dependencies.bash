2f0eefefc67f5a67f1b82b6f7241d0b57a2d9f35
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-14 21:17:42 -0800

    semantic web tools for issue #144

--- NaturalLanguageProcessing/yum-dependencies.bash
+++ NaturalLanguageProcessing/yum-dependencies.bash
@@ -11,6 +11,15 @@
 
 # Packages
 sudo yum install -y \
+  rasqal \
+  raptor \
+  redland \
+  redland-virtuoso \
+  virtuoso-opensource \
+  virtuoso-opensource-apps \
+  virtuoso-opensource-conductor \
+  virtuoso-opensource-doc \
+  virtuoso-opensource-utils \
   python-nltk \
   wordnet \
   wordnet-devel
