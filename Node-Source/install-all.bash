b7a8d4ac046ed10cfab6bb091fc369ed1503b2c4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-22 16:23:05 -0800

    install Node.js from source - needed for Shiny Server

---
+++ Node-Source/install-all.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+for i in dependencies node node-packages
+do
+  ./install-${i}.bash 2>&1 | tee ${i}.log
+done
---
+++ Node-Source/install-node.bash
@@ -0,0 +1,30 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+source ~/.bash_profile
+mkdir -p ~/local/src
+pushd ~/local/src
+export DIR=`curl -s nodejs.org/ | grep '.tar.gz' | head -n 1 | \
+  sed 's/^.*href=.*node/node/' | sed 's/.tar.gz.*$//'`
+export WHAT=${DIR}.tar.gz
+export WHERE=http://nodejs.org/dist/latest
+rm -f ${WHAT}
+curl ${WHERE}/${WHAT} > ${WHAT}
+rm -fr ${DIR}
+tar xf ${WHAT}
+
+pushd ${DIR}
+./configure
+make
+sudo make install
+popd
+
+popd
