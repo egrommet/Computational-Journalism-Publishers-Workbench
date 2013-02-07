af22f7e3f1cf3197c58d99ba4a77c3753ad0bc69
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-07 14:37:36 -0800

    install in /usr/local

--- Redis-Source/install-redis.bash
+++ Redis-Source/install-redis.bash
@@ -20,5 +20,8 @@ curl ${WHERE}/${WHAT} > ${WHAT}
 tar xf ${WHAT}
 cd ${DIR}
 patch -b src/redis-benchmark.c ${HERE}/redis-benchmark.patch
-make PREFIX=${HOME}/local install
+#make PREFIX=${HOME}/local install
+make install
+make test
+sudo make install
 popd
