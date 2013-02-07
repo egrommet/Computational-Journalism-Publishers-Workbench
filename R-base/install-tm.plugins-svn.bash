a1b6f08abdc21e26830caf335c6ad403b9982878
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-06 22:40:51 -0800

    fixed install errors

--- R-base/install-tm.plugins-svn.bash
+++ R-base/install-tm.plugins-svn.bash
@@ -9,8 +9,7 @@ svn checkout svn://scm.r-forge.r-project.org/svnroot/sentiment/
 pushd sentiment/pkg
 for i in boilerpipeR tm.plugin.sentiment tm.plugin.webmining
 do
-  R CMD build ${i}
-  R CMD INSTALL ${i}*tar.gz 2>&1 | tee ${i}.log
+  R CMD INSTALL ${i} 2>&1 | tee ${i}.log
 done
 popd
 
