9f5675ca5e0589f37ed291f5763be5e187eebf65
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-14 22:03:51 -0800

    authentication warning

--- remove-guest-tools.bash
+++ remove-guest-tools.bash
@@ -9,6 +9,9 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+echo 'Removing installed guest tools'
+echo "You will need to authenticate"
+sleep 4
 if [ -e "/usr/bin/apt-get" ]
 then
   ./apt-get-remove-guest-tools.bash
--- updateos.bash
+++ updateos.bash
@@ -9,6 +9,9 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+echo 'Updating operating system'
+echo "You will need to authenticate"
+sleep 4
 if [ -e "/usr/bin/apt-get" ]
 then
   ./apt-get-updateos.bash 2>&1 | tee updateos.log
