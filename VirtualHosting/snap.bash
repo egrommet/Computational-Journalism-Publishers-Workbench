c5d11a1ea7ffc018c3f27c81439cee8d7e0b09f3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-19 14:09:07 -0800

    Hard-coding snapshot name

--- VirtualHosting/snap.bash
+++ VirtualHosting/snap.bash
@@ -9,5 +9,5 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-sudo qemu-img snapshot -c $1 $2
-sudo qemu-img snapshot -l $2
+sudo qemu-img snapshot -c FreshInstall $1
+sudo qemu-img snapshot -l $1
