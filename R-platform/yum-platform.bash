ddd2be614044ad3174b78443584463c01d77e355
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-01 01:56:41 -0800

    ditched RKWard and refactored

--- R-platform/yum-platform.bash
+++ R-platform/yum-platform.bash
@@ -9,29 +9,38 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# RKWard and GGobi from distro!
+# R and GGobi from distro!
 sudo yum install -y \
+  R \
   R-devel \
-  rkward \
   ggobi \
   ggobi-devel
 
-# unixODBC
+# R Commander
 sudo yum install -y \
+  freeglut-devel \
   unixODBC \
   unixODBC-devel \
   unixODBC-gui-qt
 
-# R Commander, Rattle and JGR/Deducer
+# Cairo
 sudo yum install -y \
-  tcl-devel \
-  tk-devel \
-  bwidget \
-  freeglut-devel \
-  libXt-devel \
-  gtk2-devel \
-  libxml2-devel
+  libXt-devel
 
+# GUIs and packages
 ./install-packages.bash 2>&1 | tee packages.log
+
+# RStudio source
+sudo yum install -y \
+  patch
 ./install-rstudio-source.bash
+
+# RStudio desktop
+sudo yum install -y \
+  boost-devel \
+  libuuid-devel \
+  qt-devel \
+  qtwebkit-devel \
+  ant \
+  cmake
 ./install-rstudio-desktop.bash
