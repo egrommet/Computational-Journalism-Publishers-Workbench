daf181bcfd33892556bbc16eca7ed7cd67db679d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-02 01:42:10 -0800

    missing a blank line

--- R-devel/yum-dependencies.bash
+++ R-devel/yum-dependencies.bash
@@ -8,6 +8,7 @@
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
+
 sudo yum install -y \
   readline-devel \
   tcl-devel \
