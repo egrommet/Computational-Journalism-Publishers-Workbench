423d2bfb953b686979c06def591a226609c57a36
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-11 18:04:14 -0700

    three types of snapshots

---
+++ VirtualHosting/2Platform.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+sudo qemu-img snapshot -a Platform $1
