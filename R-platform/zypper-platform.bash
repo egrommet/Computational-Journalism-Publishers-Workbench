793f056d0abf551cbd86614024ad6bee5423ff53
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-19 18:41:27 -0700

    ready for smoke tests

---
+++ R-platform/zypper-platform.bash
@@ -0,0 +1,46 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+# unixODBC
+sudo -H zypper install -y \
+  unixODBC \
+  unixODBC-devel \
+  unixODBC-gui-qt
+
+# Numeric libraries
+sudo -H zypper addrepo -cf \
+  http://download.opensuse.org/repositories/devel:/libraries:/c_c++/openSUSE_12.3/devel:libraries:c_c++.repo
+sudo -H zypper addrepo -cf \
+  http://download.opensuse.org/repositories/home:/jenewton:/science/openSUSE_12.3/home:jenewton:science.repo
+sudo -H zypper --gpg-auto-import-keys refresh
+sudo -H zypper install -y \
+  gsl-devel \
+  gmp-devel \
+  mpfr-devel \
+  libatlas3-devel
+
+# GUI tools
+sudo -H zypper install -y \
+  bwidget \
+  freeglut-devel \
+  libXt-devel
+
+# RStudio source
+sudo -H zypper install -y \
+  patch
+
+# RStudio Desktop
+sudo -H zypper install -y \
+  boost-devel \
+  libuuid-devel \
+  libQtWebKit-devel \
+  ant \
+  cmake
