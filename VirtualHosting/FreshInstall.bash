e6db70b01be7da1ab9f38e1eb8f0a4d215ae7b4c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-24 15:01:17 -0700

    make sure there's only one snapshot with a given name!

--- VirtualHosting/FreshInstall.bash
+++ VirtualHosting/FreshInstall.bash
@@ -9,5 +9,6 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+sudo qemu-img snapshot -d FreshInstall $1
 sudo qemu-img snapshot -c FreshInstall $1
 sudo qemu-img snapshot -l $1
