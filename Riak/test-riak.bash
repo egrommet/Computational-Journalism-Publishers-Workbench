b1a115c1e26226703aa41b08aae37f771b94cf7f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-29 16:20:40 -0700

    separate install and test scripts for Riak

--- Riak/test-riak.bash
+++ Riak/test-riak.bash
@@ -13,12 +13,18 @@
 # http://docs.basho.com/riak/1.3.0/tutorials/fast-track/Building-a-Development-Environment/
 
 source ~/.bash_profile
-mkdir -p ~/local/
 pushd ~/local/
-rm -fr riak*
-wget http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/1.3/1.3.0/riak-1.3.0.tar.gz
-tar zxvf riak-1.3.0.tar.gz
 cd riak-1.3.0
-/usr/bin/time make all
-make devrel DEVNODES=4
+cd dev; ls
+dev1/bin/riak start
+dev2/bin/riak start
+dev3/bin/riak start
+dev4/bin/riak start
+pgrep beam
+dev2/bin/riak-admin cluster join dev1@127.0.0.1
+dev3/bin/riak-admin cluster join dev1@127.0.0.1
+dev4/bin/riak-admin cluster join dev1@127.0.0.1
+dev2/bin/riak-admin cluster plan
+dev2/bin/riak-admin cluster commit
+dev2/bin/riak-admin member-status
 popd
