21c6ec83d9a1524a4fcec512e465b33817bbb7a0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-03-29 18:27:40 -0700

    added a note on the cluster

--- Riak/test-riak.bash
+++ Riak/test-riak.bash
@@ -20,7 +20,7 @@ dev1/bin/riak start
 dev2/bin/riak start
 dev3/bin/riak start
 dev4/bin/riak start
-pgrep beam
+ps -ef | grep beam
 dev2/bin/riak-admin cluster join dev1@127.0.0.1
 dev3/bin/riak-admin cluster join dev1@127.0.0.1
 dev4/bin/riak-admin cluster join dev1@127.0.0.1
@@ -28,3 +28,6 @@ dev2/bin/riak-admin cluster plan
 dev2/bin/riak-admin cluster commit
 dev2/bin/riak-admin member-status
 popd
+
+# We should now have a four-node cluster listening on 127.0.0.1:10018, 10028, 10038, and 10048!
+# To shut it down, do 'pkill beam'
