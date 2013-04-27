591df359d8cdc89d083b8556d64cc6b695e1b2d5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-04-26 22:00:55 -0700

    only build Boost on Ubuntu - not needed on Fedora

--- Platform/install-rstudio-source.bash
+++ Platform/install-rstudio-source.bash
@@ -24,11 +24,16 @@ pushd /usr/local/src
   pushd rstudio-*
 
     pushd dependencies/common
-      for i in gwt dictionaries mathjax boost
+      for i in gwt dictionaries mathjax
       do
         patch -b ./install-$i ${HERE}/install-$i.patch
         /usr/bin/time ./install-$i
       done
+      if [ -e "/usr/bin/apt-get" ]
+      then
+        patch -b ./install-boost ${HERE}/install-boost.patch
+        /usr/bin/time ./install-boost
+      fi
     popd
 
     patch -b src/gwt/build.xml ${HERE}/build.xml.patch
