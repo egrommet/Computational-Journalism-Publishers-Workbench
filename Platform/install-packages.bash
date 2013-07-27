eeb83fd0ffd46c7e62ceedc50ac358b3714e59bc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-27 09:16:51 -0700

    use local directory for download, not /tmp

--- Platform/install-packages.bash
+++ Platform/install-packages.bash
@@ -1,1 +1,14 @@
-../common/install-packages.bash
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+source ~/.bash_profile
+mkdir -p ./downloaded_packages
+/usr/bin/time sudo -H ../common/root-install-packages.bash
