60e3d9eef71ec17ea7edc92a76427a15a6578521
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-09-23 11:03:21 -0700

    need bigger disks for building ISOs on CompJournoStick

---
+++ VirtualHosting/make-48gb-qcow2-disk.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+# Reference: http://www.ilsistemista.net/index.php/virtualization/11-kvm-io-slowness-on-rhel-6.html
+sudo qemu-img create -f qcow2 -o preallocation=metadata,size=51539607552 $1.qcow2
